aeab2ffc36031c8fad69e35106652f3ac07127c7
vasyalee <flightsho@gmail.com>
2022-03-21 15:44:37 +0300

    Added start_handler

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import telegram
 import yaml
+from telegram.ext import Updater
+import logging
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
 
-with open('config.yml') as f:
-    
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                                text="Ну привет, криптан!")
+
+with open('config.yml') as f: 
     data = yaml.load(f, Loader=yaml.FullLoader)
     
 
+updater = Updater(token=data['TOKEN'], use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                level=logging.INFO)
 
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
-bot = telegram.Bot(token=data['TOKEN'])
+updater.start_polling()
+
 

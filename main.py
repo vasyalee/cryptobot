0daeb299104bda34ca8a426e7051fb14606978d1
vasyalee <flightsho@gmail.com>
2022-03-21 16:13:21 +0300

    Added updater.idle() to stop bot

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import telegram
-import yaml
-from telegram.ext import Updater
 import logging
 from telegram import Update
-from telegram.ext import CallbackContext
-from telegram.ext import CommandHandler
+import yaml
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, 
                                 text="Ну привет, криптан!")
+
 
 with open('config.yml') as f: 
     data = yaml.load(f, Loader=yaml.FullLoader)
@@ -16,12 +25,15 @@ with open('config.yml') as f:
 
 updater = Updater(token=data['TOKEN'], use_context=True)
 dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                                level=logging.INFO)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def main():
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-updater.start_polling()
 
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
 
